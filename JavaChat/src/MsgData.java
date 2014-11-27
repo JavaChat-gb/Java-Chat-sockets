c0eef6d6c936ce69cf4667707cc4e495e1954645
tgm1314-elegenstein <elegenstein@student.tgm.ac.at>
2014-11-27 18:49:07 +0100

    Comments and so :)

--- JavaChat/src/MsgData.java
+++ JavaChat/src/MsgData.java
@@ -3,6 +3,8 @@
  */
 
 /**
+ * Dataset for Messages it contains a sendername and a message
+ * 
  * @author Erik
  *
  */
@@ -11,18 +13,31 @@ public class MsgData {
 	private String sender;
 	private String msg;
 
+	/**
+	 *
+	 * @param sender
+	 *            Sendername
+	 * @param msg
+	 *            Message
+	 */
 	public MsgData(String sender, String msg) {
 		super();
 		this.sender = sender;
 		this.msg = msg;
 	}
 
+	/**
+	 * @return the sendername
+	 */
 	public String getSender() {
 		return sender;
 	}
 
+	/**
+	 * @return the Message
+	 */
 	public String getMsg() {
 		return msg;
 	}
-	
+
 }
--- JavaChat/src/SeRe.java
+++ JavaChat/src/SeRe.java
@@ -2,9 +2,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
-
-import javax.xml.bind.DataBindingException;
 
 /**
  * 
@@ -20,6 +17,16 @@ public class SeRe {
 	private RecClient cAPP;
 	public static final int port = 9898;
 
+	/**
+	 * Sets up a standard send-recive Connection
+	 * 
+	 * @param host
+	 *            Chat-opponent's IP ex. "192.168.1.14"
+	 * @param port
+	 *            Port to connect to
+	 * @param a
+	 *            Messagehandler gets the messages delivered
+	 */
 	public SeRe(String host, int port, RecClient a) {
 		this.cAPP = a;
 
@@ -35,31 +42,48 @@ public class SeRe {
 		}
 	}
 
+	/**
+	 * Sets up a standard send-recive Connection to port 9898
+	 * 
+	 * @param host
+	 *            Chat-opponent's IP ex. "192.168.1.14"
+	 * @param a
+	 *            Messagehandler gets the messages delivered
+	 */
 	public SeRe(String host, RecClient a) {
 		this(host, port, a);
 	}
 
+	/**
+	 * Recives data from the InputStream(Sockets) and packs it into a
+	 * Data(Class) object. This object gets transported to the RecClient
+	 * specified in the Constuctor.
+	 * 
+	 * @category Send-Recive
+	 */
 	private void recive() {
 
-		try{
-		InputStream is = csocket.getInputStream();
-		String msgs="", name="", s = "";
-		for(int i ; (i = is.read())!=-1  ;s+=(char)i )
-		
-	
-		name = s.substring(0, s.indexOf(">|") + 1);
-		name.replace("<", "");
-		name.replace(">", "");
-		msgs = s.substring(s.indexOf(">|") + 2);
-		MsgData z = new MsgData(name, msgs);
+		try {
+			InputStream is = csocket.getInputStream();
+			String msgs = "", name = "", s = "";// msgs contains the message,
+												// name contains the sender
+												// name, s is a temp. var.
+			for (int i; (i = is.read()) != -1; s += (char) i)
+				// Liest zeichen vom Stream ein und fuegt diese dem string s an
 
-		cAPP.addMSG(z);
-		is.close();
-		
-		}catch(Exception e){
+				name = s.substring(0, s.indexOf(">|") + 1);
+			name.replace("<", "");
+			name.replace(">", "");
+			msgs = s.substring(s.indexOf(">|") + 2);
+			MsgData z = new MsgData(name, msgs);
+
+			cAPP.addMSG(z);
+			is.close();
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
